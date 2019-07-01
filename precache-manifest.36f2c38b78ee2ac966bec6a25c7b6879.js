7dd548f6f314774539815968e9211610440177c7
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-07-01 16:55:35 +0200

    Updates

---
+++ precache-manifest.36f2c38b78ee2ac966bec6a25c7b6879.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e4c4b3ef02e87249cf34049623a5f79",
+    "url": "/tinymce-text-editor/index.html"
+  },
+  {
+    "revision": "46874963780c58dfe855",
+    "url": "/tinymce-text-editor/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "78bed45b193db44b5796",
+    "url": "/tinymce-text-editor/static/js/2.efbca837.chunk.js"
+  },
+  {
+    "revision": "46874963780c58dfe855",
+    "url": "/tinymce-text-editor/static/js/main.8e3ae0bb.chunk.js"
+  },
+  {
+    "revision": "ff16e4386f424a55348c",
+    "url": "/tinymce-text-editor/static/js/runtime~main.99222498.js"
+  }
+]);
